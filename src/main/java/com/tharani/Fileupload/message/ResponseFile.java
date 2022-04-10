256d21f4815c37e45b3975eb19ff33fc62307b3e
unknown <tharani17082000@gmail.com>
2022-04-10 13:42:24 +0530

    getter setters

--- src/main/java/com/tharani/Fileupload/message/ResponseFile.java
+++ src/main/java/com/tharani/Fileupload/message/ResponseFile.java
@@ -1,5 +1,8 @@
 package com.tharani.Fileupload.message;
 
+import lombok.Data;
+
+@Data
 public class ResponseFile {
   private String name;
   private String url;
@@ -12,37 +15,37 @@ public class ResponseFile {
     this.type = type;
     this.size = size;
   }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getUrl() {
-    return url;
-  }
-
-  public void setUrl(String url) {
-    this.url = url;
-  }
-
-  public String getType() {
-    return type;
-  }
-
-  public void setType(String type) {
-    this.type = type;
-  }
-
-  public long getSize() {
-    return size;
-  }
-
-  public void setSize(long size) {
-    this.size = size;
-  }
+//
+//  public String getName() {
+//    return name;
+//  }
+//
+//  public void setName(String name) {
+//    this.name = name;
+//  }
+//
+//  public String getUrl() {
+//    return url;
+//  }
+//
+//  public void setUrl(String url) {
+//    this.url = url;
+//  }
+//
+//  public String getType() {
+//    return type;
+//  }
+//
+//  public void setType(String type) {
+//    this.type = type;
+//  }
+//
+//  public long getSize() {
+//    return size;
+//  }
+//
+//  public void setSize(long size) {
+//    this.size = size;
+//  }
 
 }
--- src/main/java/com/tharani/Fileupload/message/ResponseMessage.java
+++ src/main/java/com/tharani/Fileupload/message/ResponseMessage.java
@@ -1,18 +1,21 @@
 package com.tharani.Fileupload.message;
 
+import lombok.Data;
+
+@Data
 public class ResponseMessage {
   private String message;
 
   public ResponseMessage(String message) {
     this.message = message;
   }
-
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(String message) {
-    this.message = message;
-  }
+//
+//  public String getMessage() {
+//    return message;
+//  }
+//
+//  public void setMessage(String message) {
+//    this.message = message;
+//  }
 
 }
